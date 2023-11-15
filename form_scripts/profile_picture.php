2dac80c8be84ecf4714c5796d9c733e97e6455a1
uwiwiow <uwiwiow@gmail.com>
2023-11-15 13:15:44 -0600

    pfp finfo check

--- form_scripts/profile_picture.php
+++ form_scripts/profile_picture.php
@@ -2,15 +2,14 @@
 session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["profilePicture"])) {
     $targetDir = "../uploads/"; // Directorio donde se almacenarán las fotos
-    $targetFile = $targetDir . basename($_FILES["profilePicture"]["name"]);
     $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($targetFile,PATHINFO_EXTENSION));
+    $imageFileType = strtolower(pathinfo($_FILES["profilePicture"]['name'],PATHINFO_EXTENSION));
     $targetFile = $targetDir . $_SESSION['user_id'] . '.' . $imageFileType;
     $file_name = $_SESSION['user_id'] . '.' . $imageFileType;
+    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+    $tipo_mime = finfo_file($finfo, $_FILES['profilePicture']['tmp_name']);
 
-    // Verificar si el archivo es una imagen
-    $check = getimagesize($_FILES["profilePicture"]["tmp_name"]);
-    if($check !== false) {
+    if (strpos($tipo_mime, 'image') !== false) {
         $uploadOk = 1;
     } else {
         $message = urlencode("El archivo no es una imagen.");
@@ -18,24 +17,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["profilePicture"])) {
         $uploadOk = 0;
     }
 
-    // Verificar tamaño máximo
-    if ($_FILES["profilePicture"]["size"] > 3000000) {
-        $message = urlencode("El archivo es demasiado grande.");
-        header("Location: ../profile.php?error=$message");
-        $uploadOk = 0;
-    }
+    finfo_close($finfo);
 
-    // Permitir solo ciertos formatos de imagen
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
-        $message = urlencode("Solo se permiten archivos JPG, JPEG, PNG y GIF.");
+    // Verificar tamaño máximo
+    if ($_FILES["profilePicture"]["size"] > 30000000) {
+        $message = urlencode("El archivo es demasiado grande.");
         header("Location: ../profile.php?error=$message");
         $uploadOk = 0;
     }
 
     // Si todas las verificaciones son exitosas, intenta subir el archivo
     if ($uploadOk == 1) {
-
-
             try {
 
                 include('../includes/connection.php');
